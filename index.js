324bf82e21992be37322a79ba1a318f1a718c1b0
bnwyoucef <y.bounoua@esi-sba.dz>
2022-11-02 11:16:50 +0100

    home section

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let searchContainer = document.querySelector(".search-bar-container");
 let loginBtn = document.getElementById("login-btn");
 let closeFormBtn = document.getElementById("form-close");
 let loginContainer = document.querySelector(".login-form-container");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector("nav");
+let videoBtn = document.querySelectorAll(".vid-btn");
 
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
@@ -18,4 +21,18 @@ loginBtn.addEventListener("click", () => {
 });
 closeFormBtn.addEventListener("click", () => {
   loginContainer.classList.toggle("active-login");
+});
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document
+      .querySelector(".controls .active-video")
+      .classList.remove("active-video");
+    btn.classList.add("active-video");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
+  });
 });
